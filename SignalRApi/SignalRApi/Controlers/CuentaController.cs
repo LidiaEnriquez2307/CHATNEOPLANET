501cff088581688ed42d5b3ad553f8974d016a3a
Isramos279 <68452415+Isramos279@users.noreply.github.com>
2022-02-07 09:54:28 -0500

    Obtener cuentas por get

---
+++ SignalRApi/SignalRApi/Controlers/CuentaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace SignalRApi.Controlers
+{
+    [Route("[controller]")]
+    public class CuentaController : Controller
+    {
+        private string cadena = @"Server=localhost; Database=CHAT; Uid=root;";
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Modelos.Cuenta> lista = null;
+            using (var db= new MySqlConnection(cadena))
+            
+            {
+                var sql = "select * from cuenta;";
+                lista = db.Query<Modelos.Cuenta>(sql);
+            }
+
+            return Ok(lista);
+        }
+    }
+}
