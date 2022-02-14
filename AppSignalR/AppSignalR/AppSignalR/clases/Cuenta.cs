f274f1586dff7109566db669c02ccd37e8602eb6
Isramos279 <68452415+Isramos279@users.noreply.github.com>
2022-02-13 19:34:21 -0500

    Borrar AppSignalR

--- AppSignalR/AppSignalR/AppSignalR/clases/Cuenta.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppSignalR.Services
-{
-    public class Cuenta
-    {
-        public int id_cuenta { get; set; }
-        public string correo { get; set; }
-        public string contrasenia{ get; set; }
-    }
-}
