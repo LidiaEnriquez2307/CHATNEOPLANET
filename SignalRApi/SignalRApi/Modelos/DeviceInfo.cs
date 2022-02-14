bae8ac4544ddb435087d04ea51e210fe57369122
Isramos279 <68452415+Isramos279@users.noreply.github.com>
2022-02-13 19:40:30 -0500

    borrar ApiSignalR

--- SignalRApi/SignalRApi/Modelos/DeviceInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SignalRApi.SignalR
-{
-    public class DeviceInfo
-    {
-        public int Id { get; set; }
-        public string correo { get; set; }
-    }
-}
--- SignalRApi/SignalRApi/Modelos/Mensaje.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SignalRApi.SignalR
-{
-    public class Mensaje
-    {
-        public string Message { get; set; }
-        public int id_cuenta { get; set; }
-        public int id_sala { get; set; }
-    }
-}
